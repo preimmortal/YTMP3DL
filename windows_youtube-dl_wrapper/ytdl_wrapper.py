92082481f5bbef2df09d96f8e4dc2713adaf7b19
preimmortal <preimmortal@gmail.com>
2015-02-16 00:37:22 -0800

    done with initial testing

--- windows_youtube-dl_wrapper/ytdl_wrapper.py
+++ windows_youtube-dl_wrapper/ytdl_wrapper.py
@@ -13,11 +13,16 @@ while(1):
 	if(d == 'y'):
 		break
 	elif (d == 'n'):
-		raw_input ("Copy URL then press Enter\n")
+		#raw_input ("Copy URL then press Enter\n")
 		link = pyperclip.paste()
 	print "Link is: " + link
 	d = raw_input("Is the link correct? ('y' or 'n') OR (Ctrl+C to exit)\n")
-
-os.system("youtube-dl.exe --extract-audio --audio-format mp3 "+link)
+d = os.getcwd()
+#print "Current Directory: " + d
+os.chdir(d+"/bin")
+#print "New Directory: " + d
+c = "youtube-dl.exe -o ../%(playlist)s/%(title)s.%(exts)s"+" --extract-audio --audio-format mp3 "+link
+print "Executing Command: " + c
+os.system(c)
 
 raw_input("DONE")
