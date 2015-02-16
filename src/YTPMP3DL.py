ad735a16964b68c840b1ea0145709fa4c813b3a0
David <preimmortal@gmail.com>
2015-02-15 18:51:36 -0800

    added cmd line calls to vlc to open file

--- src/YTPMP3DL.py
+++ src/YTPMP3DL.py
@@ -1,6 +1,8 @@
 from lxml import html
 import requests
 
+import os
+
 # Ask for Page URL
 # raw_page = raw_input("Enter web page URL: ")
 
@@ -15,13 +17,18 @@ videos = tree.xpath('//a[@class="pl-video-title-link yt-uix-tile-link yt-uix-ses
 vidlinks = tree.xpath('//a[@class="pl-video-title-link yt-uix-tile-link yt-uix-sessionlink  spf-link "]/@href')
 
 
-
-
+# debug prints, print all vids + links
 print "Number of Videos: " + str(len(videos))
-
+linkList = list()
+YTBASEADDR = "https://www.youtube.com"
 for i in range (0, len(videos)):
     print str(i) + " " + videos[i] + " " + vidlinks[i]
+    linkList.append(YTBASEADDR+vidlinks[i])
+
+# build link list
+for l in linkList:
+    print l
 
 
-print "Try This: " + "https://www.youtube.com" + vidlinks[0]
+os.system("vlc -vvv "+linkList[0])
 
